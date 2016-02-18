eae15362befa091f4f28657532e02cf848c4f141
Bartłomiej Kurek <minus@noise-nation.net>
2016-02-18 17:54:02 +0200

    config

--- src/dnsproxy/unbound/unix.cxx
+++ src/dnsproxy/unbound/unix.cxx
@@ -22,16 +22,51 @@ std::string UnixUnbound::get_executable_name() const
 
 void UnixUnbound::start()
 {
-	std::string cmd = this->find_executable();
-	if(cmd.empty()) {
-		throw std::runtime_error("Unable to find dns proxy executable");
-	}
+	if(!api_->config.no_system_dns_proxy) {
+		std::vector<std::string> cmds = {
+			"service unbound restart",
+			"/etc/init.d/unbound restart",
+			"/etc/rc.d/unbound restart",
+			"/usr/local/etc/rc.d/unbound restart",
+			"systemctl restart unbound",
+		};
 
-	cmd.append(" -c " + config_file_path_);
-	LOG(DEBUG) << cmd << std::endl;
-	LOG(DEBUG) << "DNS Proxy config file: " << config_file_path_ << std::endl;
-	if(system(cmd.c_str()) != 0) {
-		throw std::runtime_error("Unable to start dns proxy. Command: " + cmd);
+		bool success = false;
+		for(auto const& cmd : cmds) {
+			LOG(DEBUG) << "Trying: " << cmd;
+			if(system(cmd.c_str()) == 0) {
+				success = true;
+				break;
+			}
+		}
+
+		if(!success) {
+			throw std::runtime_error(
+				"Unable to start 'unbound' system service"
+			);
+		}
+
+		std::string msg(
+			"\nSome init scripts report success even\n"
+			"when the service fails to start.\n"
+			"You may need to restart 'unbound' service yourself to be sure.\n"
+		);
+
+		// TODO: check effective uid before logging info?
+		LOG(INFO) << msg << std::endl;
+	}
+	else {
+		std::string cmd = this->find_executable();
+		if(cmd.empty()) {
+			throw std::runtime_error("Unable to find dns proxy executable");
+		}
+
+		cmd.append(" -c " + config_file_path_);
+		LOG(DEBUG) << cmd << std::endl;
+		LOG(DEBUG) << "DNS Proxy config file: " << config_file_path_ << std::endl;
+		if(system(cmd.c_str()) != 0) {
+			throw std::runtime_error("Unable to start dns proxy. Command: " + cmd);
+		}
 	}
 }
 
