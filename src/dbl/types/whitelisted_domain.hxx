d8858e5f6dd50405d63a57ec5d0c2aa4af1623fd
dbl <dbl@localhost>
2016-03-05 23:33:34 +0200

    whitelisted domains + test

---
+++ src/dbl/types/whitelisted_domain.hxx
@@ -0,0 +1,66 @@
+#ifndef DBL_TYPES_WHITELISTED_DOMAIN_HXX
+#define DBL_TYPES_WHITELISTED_DOMAIN_HXX
+
+#include "json_serializable.hxx"
+#include "dbl/db/utils/utils.hxx"
+#include <soci/soci.h>
+#include <boost/optional.hpp>
+#include <string>
+#include <ctime>
+
+namespace dbl {
+namespace types {
+
+class Domain : public JSONSerializable
+{
+public:
+	int id = int();
+	boost::optional<int> list_id = int();
+	std::string name = std::string();
+	boost::optional<std::string> description = std::string();
+	boost::optional<std::string> list_name = std::string();
+
+	void init_from_json(const Json::Value& input);
+	void to_json(Json::Value& root) const;
+};
+
+} // types
+} // dbl
+
+namespace soci {
+
+using dbl::types::Domain;
+
+template<>
+struct type_conversion<Domain>
+{
+	typedef values base_type;
+	
+	static
+	void from_base(values const& v, indicator /* ind */, Domain& r)
+	{
+		r.id = v.get<int>("id");
+		r.list_id = v.get<boost::optional<int>>("list_id", 0);
+		r.name = v.get<std::string>("name");
+		r.description = v.get<boost::optional<std::string>>("description");
+		r.list_name = v.get<boost::optional<std::string>>("list_name");
+	}
+	
+	static
+	void to_base(const Domain& r, soci::values& v, soci::indicator& ind)
+	{
+		using namespace dbl::db::utils;
+
+		v.set("id", r.id);
+		v.set("name", r.name);
+		set_optional_value(v, "list_id", r.list_id);
+		set_optional_value(v, "description", r.description);
+
+		ind = i_ok;
+	}
+};
+
+} // soci
+
+
+#endif
---
+++ test/test_domains/test_get_domains.py
@@ -0,0 +1,50 @@
+from pydbl.test.server import Server
+from pydbl.test.client import Client
+import unittest
+
+
+class TestGetDomains(unittest.TestCase):
+	def test_invalid_type(self):
+		with Server() as server:
+			client = Client(server=server)
+
+			response = client.call("get_domains", {
+				"type": "invalid"
+			})
+			self.assertFalse(response.is_ok(), "Invalid")
+
+
+	def test_blocked(self):
+		with Server() as server:
+			client = Client(server=server)
+			blocked_domains = ["testdomain.com", "example-test.com"]
+			response = client.call("block", {
+				"domains" : blocked_domains
+			})
+			self.assertTrue(response.is_ok(), "Block domains")
+
+			response = client.call("get_domains", {
+				"type": "blocked"
+			})
+			self.assertTrue(response.is_ok(), "Got blocked domains")
+			names = map(lambda d: d["name"], response.data()["domains"])
+			for domain in blocked_domains:
+				self.assertTrue(domain in names)
+
+	def test_whitelisted(self):
+		with Server() as server:
+			client = Client(server=server)
+			domains = ["testdomain.com", "example-test.com"]
+			response = client.call("unblock", {
+				"domains" : domains
+			})
+			self.assertTrue(response.is_ok(), "Unblock domains")
+
+			response = client.call("get_domains", {
+				"type": "whitelisted"
+			})
+
+			self.assertTrue(response.is_ok(), "Got whitelisted domains")
+			names = list(map(lambda d: d["name"], response.data()["domains"]))
+			for domain in domains:
+				self.assertTrue(domain in names, "Whitelisted: " + domain)
