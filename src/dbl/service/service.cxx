c89024875681718ead1ccecc9246f08fbbbd41ca
dbl <dbl@localhost>
2016-03-12 01:20:22 +0200

    cleanup

--- src/dbl/service/service.cxx
+++ src/dbl/service/service.cxx
@@ -44,51 +44,5 @@ void Service::signal_stop()
 	cv_.notify_all();
 }
 
-// void Service::stop_service()
-// {
-// 	if(api_->config.is_foreground) {
-// 		this->stop_reloader_thread();
-// 	}
-
-// 	this->stop_servers();
-// 	service_cv_.notify_all();
-// }
-
-// void Service::run_reloader_thread()
-// {
-// 	reloader_thread_ = std::thread(
-// 		[this]() {
-// 			using namespace boost::posix_time;
-// 			bool reload_signaled = false;
-// 			while(!this->reloader_stop_flag_) {
-// 				ptime t = microsec_clock::universal_time();
-// 				if(this->shm_ptr_->get_object()->reloader_semaphore.timed_wait(
-// 					   t + milliseconds(300)))
-// 				{
-// 					if(!this->reloader_stop_flag_) {
-// 						reload_signaled = true;
-// 						break;
-// 					}
-// 				}
-// 			}
-			
-// 			if(reload_signaled) {
-// 				this->reload();
-// 			}
-// 			LOG(DEBUG) << "Reloader thread finished";
-// 		}
-// 	);
-// }
-
-// void Service::stop_reloader_thread()
-// {
-// 	LOG(DEBUG) << "Stopping reloader";
-// 	reloader_stop_flag_ = true;
-// 	this->shm_ptr_->get_object()->reloader_semaphore.post();
-// 	LOG(DEBUG) << "Joining Reloader";
-// 	reloader_thread_.join();
-// 	LOG(DEBUG) << "Reloader stopped";
-// }
-
 } // service
 } // dbl
