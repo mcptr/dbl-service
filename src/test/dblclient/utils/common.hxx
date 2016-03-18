860bc2485e199a767cf22c2944e5e26ab267bd0c
dbl <dbl@localhost>
2016-03-19 01:18:44 +0200

    src/test/

---
+++ src/test/dblclient/utils/common.hxx
@@ -0,0 +1,10 @@
+#ifndef UTILS_COMMON_HXX
+#define UTILS_COMMON_HXX
+
+#define ELPP_THREAD_SAFE
+#define ELPP_DISABLE_DEFAULT_CRASH_HANDLING
+#define ELPP_STACKTRACE_ON_CRASH
+
+#include <easylogging++.h>
+
+#endif
